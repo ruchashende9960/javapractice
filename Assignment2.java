6072ecb1250fa2896713bbc8bdb947304519d7da
ruchashende9960 <159414006+ruchashende9960@users.noreply.github.com>
2024-02-08 12:37:16 +0530

    Assignment2

---
+++ Assignment2.java
@@ -0,0 +1,22 @@
+package practiceset;
+
+
+public class Assignment2{
+
+int a;
+
+float b ;
+char c;
+ byte y;     //global variable 
+short s;
+boolean t;
+ long l;
+ 
+
+public void main (String[] args){
+	
+int x;          //local variable declaration    
+int z = 50; //local variable initialization
+
+}
+}
